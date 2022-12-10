ec6f5d4b1bd57f15054598226628afef78da2546
Joey Chen <jay.hatelove@gmail.com>
2022-12-10 21:39:34 +0800

    clean up Budget class

--- BudgetProject/BudgetProject/Budget.cs
+++ BudgetProject/BudgetProject/Budget.cs
@@ -14,34 +14,34 @@ namespace BudgetProject
             Amount = amount;
         }
 
-        public int Amount { get; }
-        public string YearMonth { get; }
-
-        public DateTime GetFirstDay()
-        {
-            return DateTime.ParseExact(YearMonth, "yyyyMM", null);
-        }
-
-        public DateTime GetLastDay()
-        {
-            var firstDay = GetFirstDay();
-            var daysInMonth = DateTime.DaysInMonth(firstDay.Year, firstDay.Month);
-            return DateTime.ParseExact(YearMonth + daysInMonth, "yyyyMMdd", null);
-        }
-
-        public Period CreatePeriod()
-        {
-            return new Period(GetFirstDay(), GetLastDay());
-        }
-
-        public int GetDailyAmount()
-        {
-            return Amount / GetLastDay().Day;
-        }
+        private int Amount { get; }
+        private string YearMonth { get; }
 
         public int GetOverlappingAmount(Period period)
         {
             return GetDailyAmount() * period.GetOverlappingDays(CreatePeriod());
+        }
+
+        private Period CreatePeriod()
+        {
+            return new Period(GetFirstDay(), GetLastDay());
+        }
+
+        private int GetDailyAmount()
+        {
+            return Amount / GetLastDay().Day;
+        }
+
+        private DateTime GetFirstDay()
+        {
+            return DateTime.ParseExact(YearMonth, "yyyyMM", null);
+        }
+
+        private DateTime GetLastDay()
+        {
+            var firstDay = GetFirstDay();
+            var daysInMonth = DateTime.DaysInMonth(firstDay.Year, firstDay.Month);
+            return DateTime.ParseExact(YearMonth + daysInMonth, "yyyyMMdd", null);
         }
     }
 }
